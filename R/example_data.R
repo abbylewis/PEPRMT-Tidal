d8a6a62b17963d9a786972ac2395485fe9b31cbf
abbylewis <aslewis@vt.edu>
2026-01-27 10:13:48 -0500

    progress on package logistics

---
+++ R/example_data.R
@@ -0,0 +1,14 @@
+#' Example data
+#'
+#' Data from XXX wetlands
+#'
+#' @format ## `example_data`
+#' A data frame with 7,240 rows and 60 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source I'm not sure where these data came from
+"example_data"
