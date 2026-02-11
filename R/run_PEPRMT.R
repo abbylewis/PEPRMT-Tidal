72afbbf6f1b34ccd837753602b7f363a8f576e69
Abby <abby@serc-mp22283.us.sinet.si.edu>
2026-02-11 10:38:06 -0500

    update wrapper documentation

--- R/run_PEPRMT.R
+++ R/run_PEPRMT.R
@@ -1,19 +1,83 @@
 #' run_PEPRMT
+#' 
+#' Runs all PEPRMT functions and returns an output dataframe with modeled GPP, Reco, and CH4
 #'
-#' @param target target dataframe
+#' @param data Data frame containing 15 required columns used as model inputs.
+#'   See **Details** for expected column structure.
+#'   
+#' @description
+#' Wrapper function to run all steps of the PEPRMT model (v1.0).
+#' 
+#' @details
+#' The PEPRMT model was originally parameterized for restored freshwater
+#' wetlands in the Sacramento–San Joaquin River Delta, California, USA
+#' (Oikawa et al. 2017) and later updated for tidal wetlands with inhibition
+#' of methane production in response to salinity and nitrate
+#' (Oikawa et al. 2024).
 #'
-#' @returns filled dataframe with model results
+#' Modules are run sequentially:
+#' PEPRMT_GPP, then PEPRMT_Reco, then PEPRMT_CH4.
+#'
+#' All variables are expected at a daily time step.
+#'
+#' All PEPRMT modules use the same input structure, although not all variables
+#' are used in every module.
+#'
+#' **Expected data column order:**
+#' 1. Continuous day of year
+#' 2. Discontinuous day of year
+#' 3. Year
+#' 4. Air temperature (°C)
+#' 5. Water table depth (cm)
+#' 6. PAR (µmol m^-2 d^-1)
+#' 7. Leaf Area Index
+#' 8. Greenness Index
+#' 9. FPAR flag
+#' 10. Light Use Efficiency
+#' 11. Wetland age (years)
+#' 12. Salinity (ppt)
+#' 13. NO3 (mg L^-1)
+#' 14. Soil organic matter (g C m^-3)
+#' 15. Site identifier
+#'
+#' @returns Updated dataframe containing:
+#' \describe{
+#'   \item{GPP}{gross primary productivity
+#'     (g C CO2 m^-2 day^-1)}
+#'   \item{APAR}{absorbed photosynthetically active radiation
+#'     (umol m-2 d-1)}
+#'   \item{Reco_full}{Total ecosystem respiration
+#'     (g C CO2 m^-2 day^-1)}
+#'   \item{NEE_mod}{Net ecosystem exchange of CO2
+#'     (g C CO2 m^-2 day^-1)}
+#'   \item{S1}{Labile soil carbon pool
+#'     (g C m^-3, top meter of soil)}
+#'   \item{S2}{Soil organic carbon pool
+#'     (g C m^-3, top meter of soil)}
+#'   \item{pulse_emission_total}{total methane emitted
+#'     (g C CH4 m^-2 day^-1)}
+#'   \item{Plant_flux_net}{net methane flux via plant-mediated transport
+#'     (g C CH4 m^-2 day^-1)}
+#'   \item{Hydro_flux}{net diffusive methane flux from water to atmosphere
+#'     (g C CH4 m^-2 day^-1)}
+#'   \item{M1}{methane pool produced from labile soil carbon
+#'     (g C CH4 m^-3, top meter of soil and water)}
+#'   \item{M2}{methane pool produced from soil organic carbon
+#'     (g C CH4 m^-3, top meter of soil and water)}
+#'   \item{trans2}{fraction of methane released via plant-mediated transport
+#'     (unitless)}
+#' }
 #' @export
 #'
-run_PEPRMT <- function(target) {
-  target <- data.frame(target)
+run_PEPRMT <- function(data) {
+  data <- data.frame(data)
   #First run GPP Module
   GPP_theta <- c(0.7479271, 1.0497113, 149.4681710, 94.4532674)
-  GPP_mod_target <- PEPRMT_GPP(theta = GPP_theta, data = target)
+  GPP_mod_data <- PEPRMT_GPP(theta = GPP_theta, data = data)
   
   #Create a new dataset that included model results
-  target_results <- target |>
-    dplyr::left_join(GPP_mod_target |>
+  data_results <- data |>
+    dplyr::left_join(GPP_mod_data |>
                 dplyr::rename(GPP_mod = GPP, 
                               DOY = Time_2,
                               site = site_2),
@@ -21,16 +85,16 @@ run_PEPRMT <- function(target) {
   
   #Second run Reco Module
   #Add modeled GPP into data before running Reco module (16th column)
-  target$GPP_mod <- target_results$GPP_mod
+  data$GPP_mod <- data_results$GPP_mod
   
   Reco_theta <- c(18.41329, 1487.65701, 11.65972, 61.29611)
-  Reco_mod_target <- PEPRMT_Reco(Reco_theta, 
-                                 data = target, 
+  Reco_mod_data <- PEPRMT_Reco(Reco_theta, 
+                                 data = data, 
                                  wetland_type = 2)
   
   #Create a new dataset that included model results
-  target_results <- target_results |>
-    dplyr::left_join(Reco_mod_target |>
+  data_results <- data_results |>
+    dplyr::left_join(Reco_mod_data |>
                 dplyr::rename(DOY = Time_2, 
                               Reco_mod = Reco_full,
                               site = site_2),
@@ -38,8 +102,8 @@ run_PEPRMT <- function(target) {
   
   #Last, run CH4 module
   #Add modeled S1, S2 into data before running CH4 module (17th & 18th columns)
-  target$SOM_total <- target_results$S1
-  target$SOM_labile <- target_results$S2
+  data$SOM_total <- data_results$S1
+  data$SOM_labile <- data_results$S2
   
   CH4_theta <- c(
     #Ea_CH4_SOC kJ mol-1
@@ -60,14 +124,14 @@ run_PEPRMT <- function(target) {
     0.2
   )
   
-  CH4_mod_target <- PEPRMT_CH4(theta = CH4_theta,
-                                     data = target,
+  CH4_mod_data <- PEPRMT_CH4(theta = CH4_theta,
+                                     data = data,
                                      wetland_type = 2)
   
   #Create a new dataset that includes model results
-  target_results <- target_results |>
+  data_results <- data_results |>
     dplyr::left_join(
-      CH4_mod_target |>
+      CH4_mod_data |>
         dplyr::rename(DOY = Time_2, 
                       CH4_mod = pulse_emission_total,
                       DOY = Time_2,
@@ -75,5 +139,5 @@ run_PEPRMT <- function(target) {
       by = c("DOY", "site")
     )
   
-  return(target_results)
+  return(data_results)
 }
