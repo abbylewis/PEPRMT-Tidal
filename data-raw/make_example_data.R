0179e7585068e86909c71daae8be033595eb4f19
abbylewis <aslewis@vt.edu>
2026-02-20 13:06:13 -0500

    don't duplicate year

--- data-raw/make_example_data.R
+++ data-raw/make_example_data.R
@@ -1,7 +1,7 @@
 all_sites <- read.csv("data-raw/All_sites_master.csv")
 
 example_data <- all_sites[c(
-  "site_char", "Year", "DOY_disc", "DOY", "Year", 
+  "site_char", "Year", "DOY_disc", "DOY", 
   "TA_C", "WTD_cm", "PAR_umol_m2_day", "LAI", "EVI", "FPAR", "LUE",
   "Wetland_age_years", "Salinity_daily_ave_ppt", "NO3_mg_L", "SOM_MEM_gC_m3", 
   "CO2_gC_m2_day", "GPP_gC_m2_day", "Reco_gC_m2_day", "CH4_gC_m2_day"
